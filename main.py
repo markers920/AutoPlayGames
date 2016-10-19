cd216e614bd2f5e75192296c8bda385a37fc48a9
markers920 <markers920@gmail.com>
2016-10-18 17:16:05 -0700

    basic loop

--- main.py
+++ main.py
@@ -15,8 +15,14 @@ def main():
     #autopy.bitmap.capture_screen()                 #capture screen
     #hex(autopy.bitmap.capture_screen().get_color(1, 1)) #get capture color
     
-    image = autopy.bitmap.capture_screen()
-    get_rgb(image, 10, 10)
+    MAX_CAPTURE_INDEX = 1000
+    CAPTURE_SLEEP_TIME = 1.0
+    
+    for capture_index in range(MAX_CAPTURE_INDEX):
+        image = autopy.bitmap.capture_screen()
+        rgb = get_rgb(image, 300, 300)
+        print capture_index, float(capture_index)/MAX_CAPTURE_INDEX, rgb
+        time.sleep(CAPTURE_SLEEP_TIME)
 #END def main():
 ###############################################################################
 
@@ -24,7 +30,7 @@ def get_rgb(image, x, y):
     color = image.get_color(x, y)
     color = autopy.color.hex_to_rgb(color)
     color = [c/256.0 for c in color]
-    print color
+    return color
 #END def get_color():
 
 
